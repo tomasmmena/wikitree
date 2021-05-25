c56463bd4ad75e904567e0b81b8e51a2f1c8a268
Tomás Mena <tomasmmena@gmail.com>
2021-05-25 20:12:36 -0300

    Improved visualization through pyvis. Merge entities and use larger ngrams.

--- wikitree.py
+++ wikitree.py
@@ -4,9 +4,10 @@ Main module for the wikitree application.
 
 import argparse
 import spacy
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_lg')
 
 import wikipedia
+from pyvis.network import Network
 from wikipedia.exceptions import DisambiguationError, PageError
 from tabulate import tabulate
 
@@ -15,6 +16,12 @@ parser.add_argument('query', type=str, metavar='Q',
                     help='Query to retrieve as root from Wikipedia.')
 parser.add_argument('--depth', '-d', type=int, default=2,
                     help='Max tree depth.')
+parser.add_argument('--width', '-w', type=int, default=2,
+                    help='Neighbours to fetch for each node being expanded.')
+parser.add_argument('--single-page', action='store_true',
+                    help='Provide metrics for a single page rather than building a whole tree.')
+
+ALLOWED_LABELS = ('PERSON', )
 
 
 class RelationshipGraph(object):
@@ -32,8 +39,8 @@ class RelationshipGraph(object):
         """
         self.initial_query = query
         self.nodes = {}  # nodes indexed by key/name
-        self.edges = []  # set of tuples of three elements, the keys for the nodes at either end of the 
-                         # edge and the label for the relationship
+        self.edges = set()  # set of tuples of three elements, the keys for the nodes at either end of the 
+                            # edge and the label for the relationship
         self.depth = depth
         self.width = width
 
@@ -44,12 +51,22 @@ class RelationshipGraph(object):
         initial_node = GraphNode(self.initial_query)
         initial_node.fetch(self, self.depth, self.width)
 
-    def display(self):
-        print(tabulate(
-            self.edges,
-            headers=('From', 'To', 'Label')
-        ))
-        print('\n' + '\n'.join([_.name for _ in self.nodes.values()]))
+    def display(self, show: bool = False) -> Network:
+        """
+        Use pyvis to generate an interactive visualization of this instance of the relationship graph.
+
+        :param show: If set to True the pyvis network will be exported to an HTML document and openned in 
+            the browser.
+        """
+        network = Network()
+        network.add_nodes(list(self.nodes.keys()))
+        network.add_edges([t[:2] for t in self.edges])
+
+        if show:
+            network.toggle_physics(True)
+            network.show('output.html')
+        
+        return network
 
 
 class GraphNode(object):
@@ -67,6 +84,9 @@ class GraphNode(object):
         self.page = None
         self.name = None
 
+        self.all_entities = None
+        self.selected_entities = None
+
     def fetch(self, graph: RelationshipGraph, depth: int = 2, width: int = 2):
         """
         Retrive information for this node in the graph from Wikipedia. Determine candidates for adjacent 
@@ -78,12 +98,21 @@ class GraphNode(object):
         try:
             self.page = wikipedia.page(self.query, auto_suggest=False)
         except DisambiguationError as err:
-            print(f'Disambiguating to {err.args[1][0]}')
-            self.page = wikipedia.page(err.args[1][0], auto_suggest=False)
+            for alternative in err.args[1]:
+                if '(name)' in alternative or '(surname)' in alternative or '(given name)' in alternative or '(disambiguation)' in alternative:
+                    continue
+                print(f'Disambiguating to {alternative}')
+                self.page = wikipedia.page(alternative, auto_suggest=False)
+                break
+            else:
+                print(f'Disambiguating to {err.args[1][0]}')
+                self.page = wikipedia.page(err.args[1][0], auto_suggest=False)
+            
         self.name = self.page.title
 
         graph.nodes[self.name] = self
         
+        processed = set()
         if depth > 0:
             # Extract entities
             entities = nlp(self.page.content).ents  # Entities extracted from the text
@@ -92,21 +121,41 @@ class GraphNode(object):
             for e in entities:
                 entity_counts[(e.text, e.label_)] = entity_counts.get((e.text, e.label_), 0) + 1
 
+            self.entities = entity_counts
+
             # Select entities
-            labels = ('PERSON', )
-            candidate_entities = [k[0] for k, v in sorted(entity_counts.items(), key=lambda _: _[1]) if k[1] in labels]
+            candidate_entities = [k[0] for k, v in sorted(entity_counts.items(), key=lambda _: _[1]) if k[1] in ALLOWED_LABELS]
             selected_entities = []
             while candidate_entities and len(selected_entities) < width:
                 candidate = candidate_entities.pop()
+
+                # Promotion logic: if there is a bigram, trigram or ngram further down the list that contains the value, we promote it
+                # to be processed in place of the current candidate. We add it to a set of already processed candidates so as not to process it again.
+                if candidate in processed:
+                    continue
+                for processed_candidate in processed:
+                    if candidate.lower() in processed_candidate.lower():
+                        continue
+                if len(candidate.split(' ')) == 1:
+                    for other_candidate in reversed(candidate_entities):
+                        # print(candidate, other_candidate)
+                        if candidate.lower() in other_candidate.lower() and len(other_candidate.split(' ')) > 1:
+                            print(f'Promoting {other_candidate} in place of {candidate}.')
+                            candidate = other_candidate
+                            break
+                processed.add(candidate)
+                
                 try:
                     page = wikipedia.page(candidate, auto_suggest=False)
                 except DisambiguationError as err:
                     page = wikipedia.page(err.args[1][0], auto_suggest=False)
-                except PageError:
+                except (PageError, KeyError):  # KeyError controls for an internal error in the wikipedia client.
                     continue
                 print(f'{candidate} -> {page.title}')
                 if page.title != self.page.title and page.title not in graph.nodes:
                     selected_entities.append(candidate)
+
+            self.selected_entities = selected_entities
 
             # Get selected entitites
             for query in selected_entities:
@@ -116,14 +165,23 @@ class GraphNode(object):
                     node = GraphNode(query)
                     node.fetch(graph, depth=depth - 1, width=width)
                 
-                graph.edges.append((self.name, node.name, 'UNK'))
+                graph.edges.add((*sorted([self.name, node.name]), 'UNK'))
+
+    def summary(self):
+        print('\nEntities:\n')
+        print(tabulate([(k[0], k[1], v) for k, v in sorted(self.entities.items(), key=lambda _: _[1], reverse=True) if k[1] in ALLOWED_LABELS]))
 
 
 if __name__ == '__main__':
     print('Welcome to Wikitree!')
     args = parser.parse_args()
 
-    graph = RelationshipGraph(args.query)
-    graph.fetch()
+    if not args.single_page:
+        graph = RelationshipGraph(args.query, depth=args.depth, width=args.width)
+        graph.fetch()
 
-    graph.display()
+        graph.display(show=True)
+    else:
+        graph = RelationshipGraph(args.query, depth=1)
+        graph.fetch()
+        list(graph.nodes.values())[0].summary()
