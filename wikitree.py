b422cccc07215fc54c1c7e3ec124544681a371e0
Tomás Mena <tomasmmena@gmail.com>
2021-05-31 20:03:07 -0300

    Add support for ORG and LOC nodes. Color nodes according to type.

--- wikitree.py
+++ wikitree.py
@@ -23,6 +23,8 @@ parser.add_argument('--query', '-q', type=str,
                     help='Query to retrieve as root from Wikipedia.')
 parser.add_argument('--session', '-s', type=str,
                     help='Name of the session.')
+parser.add_argument('--label', '-l', type=str, default='PER',
+                    help='Label for the initial node.')
 parser.add_argument('--depth', '-d', type=int, default=2,
                     help='Max tree depth.')
 parser.add_argument('--width', '-w', type=int, default=2,
@@ -30,7 +32,12 @@ parser.add_argument('--width', '-w', type=int, default=2,
 parser.add_argument('--single-page', action='store_true',
                     help='Provide metrics for a single page rather than building a whole tree.')
 
-ALLOWED_LABELS = ('PER', )
+ALLOWED_LABELS = ('PER', 'ORG', 'LOC')
+NODE_COLORS = ('#f8ffe5', 
+               '#06d6a0',
+               '#1b9aaa',
+               '#ef476f',
+               '#ef476f')
 
 
 class RelationshipGraph(object):
@@ -38,7 +45,7 @@ class RelationshipGraph(object):
     This class represents a relationship graph containing multiple nodes and the edges connecting them.
     """
 
-    def __init__(self, query: str, depth: int = 2, width: int = 2) -> None:
+    def __init__(self, query: str, depth: int = 2, width: int = 2, initial_label = 'PER') -> None:
         """
         Initialize an empty graph with an initial query.
 
@@ -47,6 +54,7 @@ class RelationshipGraph(object):
             the page is different.
         """
         self.initial_query = query
+        self.initial_label = initial_label
         self.nodes = {}     # nodes indexed by key/name
         self.edges = set()  # set of tuples of three elements, the keys for the nodes at either end of the 
                             # edge and the label for the relationship
@@ -57,7 +65,7 @@ class RelationshipGraph(object):
         """
         Fetch nodes and their relationships from Wikipedia.
         """
-        initial_node = GraphNode(self.initial_query)
+        initial_node = GraphNode(self.initial_query, node_type=self.initial_label)
         initial_node.fetch(self, self.depth, self.width)
 
     def display(self, show: bool = False) -> Network:
@@ -67,6 +75,8 @@ class RelationshipGraph(object):
         :param show: If set to True the pyvis network will be exported to an HTML document and openned in 
             the browser.
         """
+        color_theme = {k: v for k, v in zip(ALLOWED_LABELS, NODE_COLORS)}
+
         network = Network(
             height='100%',
             width='100%'
@@ -75,7 +85,8 @@ class RelationshipGraph(object):
             network.add_node(
                 k,
                 label=k,
-                title=v.label()
+                title=v.label(),
+                color=color_theme[v.node_type]
             )
         network.add_edges([t[:2] for t in self.edges])
 
@@ -91,7 +102,7 @@ class GraphNode(object):
     This class represents a node in the concept graph.
     """
 
-    def __init__(self, query):
+    def __init__(self, query: str, node_type: str = None):
         """
         Initialize graph node.
 
@@ -100,6 +111,7 @@ class GraphNode(object):
         self.query = query
         self.page = None
         self.name = None
+        self.node_type = node_type
 
         self.all_entities = None
         self.selected_entities = None
@@ -117,7 +129,7 @@ class GraphNode(object):
             sw = set(stopwords.words('english')) | {'born'}  # stopwords
             candidate = None
             max_count = 0
-            if hint_text is not None:
+            if hint_text is not None and self.node_type == 'PER':
                 for alternative in err.args[1]:
                     if '(name)' in alternative or '(surname)' in alternative or '(given name)' in alternative or '(disambiguation)' in alternative:
                         continue
@@ -170,24 +182,26 @@ class GraphNode(object):
             candidate_entities = [k for k, v in sorted(entity_counts.items(), key=lambda _: _[1]) if k[1] in ALLOWED_LABELS]
             selected_entities = []
             linked_entities = []
-            while candidate_entities and len(selected_entities) + len(linked_entities) < width:
+            while depth > 0 and candidate_entities and (len(selected_entities) + len(linked_entities) < width or len([_ for _ in selected_entities if _[1] == 'PER']) == 0):
                 candidate, label = candidate_entities.pop()
                 if '##' in candidate:
                     continue
 
                 # Promotion logic: if there is a bigram, trigram or ngram further down the list that contains the value, we promote it
                 # to be processed in place of the current candidate. We add it to a set of already processed candidates so as not to process it again.
-                if candidate in processed:
-                    continue
-                for processed_candidate in processed:
-                    if candidate.lower() in processed_candidate.lower():
+                if label == 'PER':
+                    if candidate in processed:
                         continue
-                if len(candidate.split(' ')) == 1:
-                    for other_candidate, label in reversed(candidate_entities):
-                        if candidate.lower() in other_candidate.lower() and len(other_candidate.split(' ')) > 1:
-                            print(f'Promoting {other_candidate} in place of {candidate}.')
-                            candidate = other_candidate
-                            break
+                    for processed_candidate in processed:
+                        if candidate.lower() in processed_candidate.lower():
+                            continue
+                    if len(candidate.split(' ')) == 1:
+                        for other_candidate, label in reversed(candidate_entities):
+                            if candidate.lower() in other_candidate.lower() and len(other_candidate.split(' ')) > 1:
+                                print(f'Promoting {other_candidate} in place of {candidate}.')
+                                candidate = other_candidate
+                                break
+
                 processed.add(candidate)
                 
                 try:
@@ -199,7 +213,7 @@ class GraphNode(object):
                     continue
 
                 if page.title != self.page.title and page.title not in graph.nodes:
-                    selected_entities.append(candidate)
+                    selected_entities.append((candidate, label))
                 elif page.title != self.page.title:
                     linked_entities.append(candidate)
                     graph.edges.add((*sorted([self.name, page.title]), 'UNK'))
@@ -207,12 +221,12 @@ class GraphNode(object):
             self.selected_entities = selected_entities
 
             # Get selected entitites
-            for query in selected_entities:
+            for query, label in selected_entities:
                 if query in graph.nodes:
                     node = graph.nodes.get(query)
                 else:
-                    node = GraphNode(query)
-                    node.fetch(graph, depth=depth - 1, width=width, hint_text=self.page.content)
+                    node = GraphNode(query, node_type=label)
+                    node.fetch(graph, depth=depth - 1 if label == 'PER' else 0, width=width, hint_text=self.page.content)
                 
                 graph.edges.add((*sorted([self.name, node.name]), 'UNK'))
 
@@ -251,7 +265,7 @@ if __name__ == '__main__':
         ner = pipeline('ner', grouped_entities=True)
         if args.single_page:
             # Just load a single node graph and show debugging information.
-            graph = RelationshipGraph(args.query, depth=1)
+            graph = RelationshipGraph(args.query, depth=1, initial_label=args.label)
             graph.fetch()
             list(graph.nodes.values())[0].summary()
         elif args.session is not None:
@@ -259,10 +273,10 @@ if __name__ == '__main__':
             if session_path.is_file():
                 with open(session_path.as_posix(), 'rb') as f:
                     graph = pickle.load(f)
-                new_node = graph.nodes.get(args.query, None) or GraphNode(args.query)
+                new_node = graph.nodes.get(args.query, None) or GraphNode(args.query, node_type=args.label)
                 new_node.fetch(graph, args.depth, args.width)
             else:
-                graph = RelationshipGraph(args.query, depth=args.depth, width=args.width)
+                graph = RelationshipGraph(args.query, depth=args.depth, width=args.width, initial_label=args.label)
                 graph.fetch()
 
             graph.display(show=True)
@@ -275,7 +289,7 @@ if __name__ == '__main__':
                     pickle.dump(graph, f)
             
         else:
-            graph = RelationshipGraph(args.query, depth=args.depth, width=args.width)
+            graph = RelationshipGraph(args.query, depth=args.depth, width=args.width, label=args.label)
             graph.fetch()
 
             graph.display(show=True)
